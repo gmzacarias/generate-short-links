c3aa3388b4a81fc600ff3fba3adaff5bac4127a8
gmzacarias <gastonmzacarias@gmail.com>
2025-03-05 16:54:42 -0300

    Create - se creo la class Url

---
+++ model/url/url.ts
@@ -0,0 +1,22 @@
+import { Database } from "@/types/supabase-types"
+
+
+class Url {
+    id?: string;
+    url: string;
+    shortUrl?: string | null = null;
+    createdAt?: Date;
+    constructor(id: string, url: string, shortUrl: string, createdAt: Date) {
+        if (!url) {
+            throw new Error("Ingresar una url valida")
+        }
+        this.url = url;
+        this.createdAt = new Date;
+    }
+
+    static async shortenUrl(url: string) {
+
+    }
+
+
+}
