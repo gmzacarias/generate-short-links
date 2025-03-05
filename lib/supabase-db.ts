7a9cedf2428bd25b0659dd3504b3d2f7c66085a0
gmzacarias <gastonmzacarias@gmail.com>
2025-03-05 15:55:11 -0300

    FIX - se modifico anon key por service role key para tener mas permisos

--- lib/supabase-db.ts
+++ lib/supabase-db.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl: string = process.env.SUPABASE_URL!
-const supabaseKey: string = process.env.SUPABASE_KEY!
+const supabaseKey: string = process.env.SUPABASE_SERVICE_ROLE_KEY!
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 
