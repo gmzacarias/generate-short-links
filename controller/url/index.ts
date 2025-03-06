50d10a3ff9ea6d83d92f80754ef815608f91a8e9
gmzacarias <gastonmzacarias@gmail.com>
2025-03-06 17:05:03 -0300

    Create - se creo las urls segun el entorno de desarrollo

--- controller/url/index.ts
+++ controller/url/index.ts
@@ -1,8 +1,23 @@
 import { Url } from "@/model/url"
+enum Environment {
+    Development = "development",
+    Production = "production",
+    Test = "test"
+}
+const currentEnvironment = process.env.NODE_ENV
+let BASE_URL: string
 
-async function generateLink(url:string) {
+if (currentEnvironment === Environment.Development) {
+    BASE_URL = "https://localhost:3000"
+} else if (currentEnvironment === Environment.Production) {
+    BASE_URL = "https://google.com.ar"
+} else if (currentEnvironment === Environment.Test) {
+    BASE_URL = "https://testapi.com"
+}
 
-    
+async function generateLink(url: string) {
 
-    
+
+
+
 }
