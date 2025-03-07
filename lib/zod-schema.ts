a94a56d257780fc64e2e175edb48353bee1e0f7b
gmzacarias <gastonmzacarias@gmail.com>
2025-03-07 20:16:16 -0300

    Create - se creo el schemaRedirectUrl para validar el code en el endpoint

--- lib/zod-schema.ts
+++ lib/zod-schema.ts
@@ -7,3 +7,7 @@ export const schemaCreateUrl =z.object({
 export const schemaDeleteId=z.object({
     id:z.string().min(1).regex(/^\d+$/,{message:"el id solo debe contener numeros de 0 al 9"})
 }).strict({message:"no se pueden agregar campos adicionales"})
+
+export const schemaRedirectUrl=z.object({
+code:z.string().length(6,{message:"el code debe tener 6 caracteres"}),
+}).strict({message:"no se pueden agregar campos adicionales"})
